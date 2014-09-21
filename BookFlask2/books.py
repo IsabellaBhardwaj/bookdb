162b94cbe7f8a6779cb5ff41b9538d82c6b55ee7
Isabella Bhardwaj <isabella.bhardwaj@gmail.com>
2014-09-20 22:30:44 -0500

    'Updated'

--- BookFlask2/books.py
+++ BookFlask2/books.py
@@ -10,57 +10,89 @@ books = database.mybooks
 #Homepage
 @app.route('/')
 def splash():
-    return app.send_static_file('splash.html')
+	return app.send_static_file('splash.html')
 
 #Individual information page for each book
 @app.route('/detail/<title>/<author>/')
 def detail(title, author):
-    return render_template('detail.html', result=books.find_one({'title':title, 'author':author}))
+	cursor = books.find_one({'title':title, 'author':author})
+	results = {field:value for field, value in cursor.items()}
+	return render_template('detail.html', result=results)
 
 #serves image in image file for a particular book
 @app.route('/static/images/<image>/')
 def image(image):
-    return app.send_static_file('images/'+image)
+	return app.send_static_file('images/'+image)
 
 #Leads to detail page of a randomly chosen book
 @app.route('/featured/')
 def featured():
-    random = books.find_one()
-    return redirect('/detail/'+random['title']+'/'+random['author']+'/')
+	random = books.find_one()
+	return redirect('/detail/'+random['title']+'/'+random['author']+'/')
 
 
 #The search page
 @app.route('/search/', methods=['GET', 'POST'])
 def search():
-    #Return results for titles, authors and genres that match the search query
-    if request.method == 'POST':
-        query = request.form['query']
-        return render_template('search.html', posting=True, query=query, title_results = books.find({'title':query}),
-                               author_results = books.find({'author':query}), genre_results = books.find({'genre':query}))  
-    else:
-        return render_template('search.html', posting=False)
+	#Return results for titles, authors and genres that match the search query
+	if request.method == 'POST':
+		query = request.form['query']
+		title_cursor = books.find({'title':query})
+		author_cursor = books.find({'author':query})
+		genre_cursor = books.find({'genre':query})
+
+		no_results = author_cursor.count() == 0 and title_cursor.count() == 0 and genre_cursor.count() == 0
+	
+		title_dict = {}
+		for element in title_cursor:
+			inner_dict = {} 
+			for key in element:
+				inner_dict[key] = element[key]	
+			title_dict[element['_id']]= inner_dict
+
+		author_dict = {}
+		for element in author_cursor:
+			inner_dict = {} 
+			for key in element:
+				inner_dict[key] = element[key]
+			author_dict[element['_id']] = inner_dict
+		
+
+		genre_dict = {}
+		for element in genre_cursor:
+			inner_dict = {} 
+			for key in element:
+				inner_dict[key] = element[key]
+			genre_dict[element['_id']] = inner_dict
+		
+
+      
+
+		return render_template('search.html', posting=True, query=query, no_results=no_results, title_results=title_dict, author_results=author_dict, genre_results=genre_dict)  
+	else:
+		return render_template('search.html', posting=False)
 
 
 #The page to add a book to the database
 @app.route('/add/', methods=['GET', 'POST'])
 def add():
-    if request.method == 'POST':
-        new_data = {k : v for k, v in request.form.items()}
-        #If the user leaves a field blank
-        if new_data['title'] == '' or new_data['author'] == '' or new_data['genre'] == '' or new_data['description'] == '':
-            return render_template('add.html', alert="required")
-        #If the user tries to add a book that's already in the database
-        elif books.find({'title':new_data['title'], 'author':new_data['author']}).count() > 0: 
-            return render_template('add.html', alert="exists")
-        else:
-            books.insert(new_data)
-            return render_template('add.html', alert = "success")
-    else:
-        return render_template('add.html', alert="")
+	if request.method == 'POST':
+		new_data = {k : v for k, v in request.form.items()}
+		#If the user leaves a field blank
+		if new_data['title'] == '' or new_data['author'] == '' or new_data['genre'] == '' or new_data['description'] == '':
+			return render_template('add.html', alert="required")
+		#If the user tries to add a book that's already in the database
+		elif books.find({'title':new_data['title'], 'author':new_data['author']}).count() > 0: 
+			return render_template('add.html', alert="exists")
+		else:
+			books.insert(new_data)
+			return render_template('add.html', alert = "success")
+	else:
+		return render_template('add.html', alert="")
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+	app.debug = True
+	app.run()
 
 
 
