c3bef4d1ebc249e0f79af69f94c22666be0a0448
Isabella Bhardwaj <isabella.bhardwaj@gmail.com>
2014-12-01 20:54:50 -0600

    Added 'featured' page to cassandra, but it currently pulls up the same page all the time.

--- BookFlask2/books_cass.py
+++ BookFlask2/books_cass.py
@@ -25,10 +25,8 @@ def splash():
 def featured():
 	query = "SELECT id FROM "+table_name+" LIMIT 1"
 	row = session.execute(query)[0]
-	print(row)
 	id = row.id
-	print("Id:"+str(id))
-	return redirect('/detail/'+str(row.id)+'/')
+	return redirect('/detail/'+str(id)+'/')
 	
 
 #The page to add a book to the database
